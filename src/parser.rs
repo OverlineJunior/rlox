ba48b54e81304b6f5b7a5526fac6f14bd291852f
over <franciscosebastianyjunior@hotmail.com>
2024-03-07 00:41:42 -0300

    Replace Cursor with VecDeque

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use crate::{
     cursor::Cursor, expr::Expr, literal::Literal, token::Token, token_kind::TokenKind as TK,
 };
@@ -7,13 +9,12 @@ use crate::{
 macro_rules! binary_expr {
     ($($name:ident -> $left:ident ($($op:ident),+) $right:ident)*) => {
         $(
-            fn $name(tokens: &mut Cursor<Token>) -> Option<Expr> {
+            fn $name(tokens: &mut VecDeque<Token>) -> Option<Expr> {
                 let mut expr = $left(tokens)?;
 
-                while let Some(op) = tokens.current().filter(|c| {
-                    $(c.kind == TK::$op)||+
-                }) {
-                    tokens.eat().expect("Should have a token");
+                while tokens.front().filter(|c| { $(c.kind == TK::$op)||+ }).is_some() {
+                    let op = tokens.front().expect("Should have a token").clone();
+                    tokens.pop_front().expect("Should have a token");
                     let right = $right(tokens)?;
                     expr = Expr::Binary(Box::new(expr), op, Box::new(right));
                 }
@@ -24,7 +25,7 @@ macro_rules! binary_expr {
     };
 }
 
-fn expression(tokens: &mut Cursor<Token>) -> Option<Expr> {
+fn expression(tokens: &mut VecDeque<Token>) -> Option<Expr> {
     equality(tokens)
 }
 
@@ -35,11 +36,11 @@ binary_expr! {
     factor -> unary (Star, Slash) unary
 }
 
-fn unary(tokens: &mut Cursor<Token>) -> Option<Expr> {
-    let tk = tokens.current()?.kind;
+fn unary(tokens: &mut VecDeque<Token>) -> Option<Expr> {
+    let tk = tokens.front()?.kind;
 
     if tk == TK::Bang || tk == TK::Minus {
-        let op = tokens.eat().expect("Should have a token");
+        let op = tokens.pop_front().expect("Should have a token");
         let right = unary(tokens)?;
         return Some(Expr::Unary(op, Box::new(right)));
     }
@@ -47,20 +48,23 @@ fn unary(tokens: &mut Cursor<Token>) -> Option<Expr> {
     primary(tokens)
 }
 
-fn primary(tokens: &mut Cursor<Token>) -> Option<Expr> {
-    if tokens.current()?.kind == TK::LeftParenthesis {
-        tokens.eat().expect("Should have a token");
+fn primary(tokens: &mut VecDeque<Token>) -> Option<Expr> {
+    if tokens.front()?.kind == TK::LeftParenthesis {
+        tokens.pop_front().expect("Should have a token");
         let expr = expression(tokens)?;
+
+        let t = tokens.pop_front().expect("Missing closing parenthesis");
         assert_eq!(
-            tokens.eat().expect("Missing closing parenthesis").kind,
+            t.kind,
             TK::RightParenthesis,
             "Expected closing parenthesis, got {:#?}",
-            tokens.prev()
+            t
         );
+
         return Some(Expr::Group(Box::new(expr)));
     }
 
-    let tok = tokens.eat()?;
+    let tok = tokens.pop_front()?;
 
     let lit = match tok.kind {
         TK::True => Some(true.into()),
@@ -79,15 +83,15 @@ fn primary(tokens: &mut Cursor<Token>) -> Option<Expr> {
 }
 
 mod tests {
-    use std::{fs, path::Path};
     use crate::{cursor::Cursor, scanner::tokenize, token::Token, token_kind::TokenKind as TK};
+    use std::{collections::VecDeque, fs, path::Path};
 
     use super::expression;
 
     #[test]
     fn test() {
         // 2 * (4 + -6)
-        let tokens = vec![
+        let mut tokens = VecDeque::from([
             Token::new(TK::Number, "2".into(), 2.0.into(), 1),
             Token::symbol(TK::Star, "*".into(), 1),
             Token::symbol(TK::LeftParenthesis, "(".into(), 1),
@@ -96,9 +100,8 @@ mod tests {
             Token::symbol(TK::Minus, "-".into(), 1),
             Token::new(TK::Number, "6".into(), 6.0.into(), 1),
             Token::symbol(TK::RightParenthesis, ")".into(), 1),
-        ];
-        let mut cursor = Cursor::new(tokens);
-        let expr = expression(&mut cursor).expect("Should have an expression");
+        ]);
+        let expr = expression(&mut tokens).expect("Should have an expression");
         assert_eq!(expr.to_string(), "(* 2 (group (+ 4 (- 6))))");
     }
 }
