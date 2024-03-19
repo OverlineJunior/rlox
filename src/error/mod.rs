794621e93059494ccb0de5c1ef92e338c867bbcc
over <franciscosebastianyjunior@hotmail.com>
2024-03-18 21:43:33 -0300

    Implement Error, which englobes scan and parse errors

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,2 +1,39 @@
-pub mod scan_error;
 pub mod parse_error;
+pub mod scan_error;
+
+use std::fmt;
+
+use parse_error::ParseError;
+use scan_error::ScanError;
+
+pub enum Error {
+    Scan(ScanError),
+    Parse(ParseError),
+}
+
+impl From<ScanError> for Error {
+    fn from(err: ScanError) -> Self {
+        Error::Scan(err)
+    }
+}
+
+impl From<ParseError> for Error {
+    fn from(err: ParseError) -> Self {
+        Error::Parse(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Scan(err) => write!(f, "{err}"),
+            Error::Parse(err) => write!(f, "{err}"),
+        }
+    }
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
+}
