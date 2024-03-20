f5c29bde58f7d5359d7e9bb482ae099b91c5a28e
over <franciscosebastianyjunior@hotmail.com>
2024-03-20 19:15:25 -0300

    Implement expect methods for Literal

--- src/literal.rs
+++ src/literal.rs
@@ -8,6 +8,42 @@ pub enum Literal {
     Nil,
 }
 
+impl Literal {
+    pub fn expect_number(&self) -> f64 {
+        match self {
+            Self::Number(n) => *n,
+            v => panic!(
+                "Expected `Literal` variant to be `Number`, but is `{:?}`",
+                v
+            ),
+        }
+    }
+
+    pub fn expect_string(&self) -> &str {
+        match self {
+            Self::String(s) => s,
+            v => panic!(
+                "Expected `Literal` variant to be `String`, but is `{:?}`",
+                v
+            ),
+        }
+    }
+
+    pub fn expect_bool(&self) -> bool {
+        match self {
+            Self::Bool(b) => *b,
+            v => panic!("Expected `Literal` variant to be `Bool`, but is `{:?}`", v),
+        }
+    }
+
+    pub fn expect_nil(&self) {
+        match self {
+            Self::Nil => (),
+            _ => panic!("Expected `Literal` variant to be `Nil`"),
+        }
+    }
+}
+
 impl From<f64> for Literal {
     fn from(n: f64) -> Self {
         Literal::Number(n)
