affd6990c617771ad51b20ae889bdb2ab65b1136
over <franciscosebastianyjunior@hotmail.com>
2024-04-16 04:18:48 -0300

    Replace panics with RuntimeError results

--- src/expr.rs
+++ src/expr.rs
@@ -1,4 +1,9 @@
-use crate::{literal::Literal, token::Token, token_kind::TokenKind as TK};
+use crate::{
+    error::runtime_error::{bad_bin_ops, bad_un_op, RuntimeError},
+    literal::Literal,
+    token::Token,
+    token_kind::TokenKind as TK,
+};
 
 pub enum Expr {
     Literal(Literal),
@@ -9,95 +14,98 @@ pub enum Expr {
 }
 
 impl Expr {
-    pub fn eval(&self) -> Literal {
+    pub fn eval(&self) -> Result<Literal, RuntimeError> {
         match self {
-            Expr::Literal(literal) => literal.clone(),
+            Expr::Literal(literal) => Ok(literal.clone()),
 
             Expr::Unary(op, r) => {
-                let r = r.eval();
+                let r = r.eval()?;
 
                 match op.kind {
                     TK::Minus => match r {
-                        Literal::Number(n) => Literal::Number(-n),
-                        _ => panic!("Unary minus can only be applied to numbers"),
+                        Literal::Number(n) => Ok(Literal::Number(-n)),
+
+                        _ => Err(bad_un_op(op.kind, r, op.line)),
                     },
 
-                    _ => panic!("Invalid unary operator"),
+                    _ => panic!("Invalid unary operator `{:?}`", op.kind),
                 }
             }
 
             Expr::Binary(l, op, r) => {
-                let l = l.eval();
-                let r = r.eval();
+                let l = l.eval()?;
+                let r = r.eval()?;
 
                 match op.kind {
-                    TK::Plus => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Number(l + r),
+                    TK::Plus => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Number(l + r)),
+
                         (Literal::String(l), Literal::String(r)) => {
-                            Literal::String(format!("{}{}", l, r))
+                            Ok(Literal::String(format!("{}{}", l, r)))
                         }
-                        _ => panic!("Operands must be two numbers or two strings"),
+
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::Minus => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Number(l - r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::Minus => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Number(l - r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::Star => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Number(l * r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::Star => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Number(l * r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::Slash => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Number(l / r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::Slash => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Number(l / r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::Greater => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Bool(l > r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::Greater => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Bool(l > r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::GreaterEqual => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Bool(l >= r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::GreaterEqual => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Bool(l >= r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::Less => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Bool(l < r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::Less => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Bool(l < r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::LessEqual => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Bool(l <= r),
-                        _ => panic!("Operands must be numbers"),
+                    TK::LessEqual => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Bool(l <= r)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::EqualEqual => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Bool(l == r),
-                        (Literal::String(l), Literal::String(r)) => Literal::Bool(l == r),
-                        (Literal::Bool(l), Literal::Bool(r)) => Literal::Bool(l == r),
-                        (Literal::Nil, Literal::Nil) => Literal::Bool(true),
-                        _ => panic!("Operands must be two numbers, two strings, or two booleans"),
+                    TK::EqualEqual => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Bool(l == r)),
+                        (Literal::String(l), Literal::String(r)) => Ok(Literal::Bool(l == r)),
+                        (Literal::Bool(l), Literal::Bool(r)) => Ok(Literal::Bool(l == r)),
+                        (Literal::Nil, Literal::Nil) => Ok(Literal::Bool(true)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    TK::BangEqual => match (l, r) {
-                        (Literal::Number(l), Literal::Number(r)) => Literal::Bool(l != r),
-                        (Literal::String(l), Literal::String(r)) => Literal::Bool(l != r),
-                        (Literal::Bool(l), Literal::Bool(r)) => Literal::Bool(l != r),
-                        (Literal::Nil, Literal::Nil) => Literal::Bool(false),
-                        _ => panic!("Operands must be two numbers, two strings, or two booleans"),
+                    TK::BangEqual => match (&l, &r) {
+                        (Literal::Number(l), Literal::Number(r)) => Ok(Literal::Bool(l != r)),
+                        (Literal::String(l), Literal::String(r)) => Ok(Literal::Bool(l != r)),
+                        (Literal::Bool(l), Literal::Bool(r)) => Ok(Literal::Bool(l != r)),
+                        (Literal::Nil, Literal::Nil) => Ok(Literal::Bool(false)),
+                        _ => Err(bad_bin_ops(op.kind, l, r, op.line)),
                     },
 
-                    _ => panic!("Invalid binary operator"),
+                    _ => panic!("Invalid binary operator `{:?}`", op.kind),
                 }
             }
 
             Expr::Group(expr) => expr.eval(),
 
             Expr::Ternary(expr, if_, else_) => {
-                let cond = expr.eval();
+                let cond = expr.eval()?;
 
                 if cond.is_truthy() {
                     if_.eval()
