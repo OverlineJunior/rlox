9632c3726059b10f535a0035f67ef95cf07c194b
over <franciscosebastianyjunior@hotmail.com>
2024-04-16 04:16:45 -0300

    Add new runtime error BadUnOp and rename old one

--- src/error/runtime_error.rs
+++ src/error/runtime_error.rs
@@ -4,7 +4,12 @@ use crate::{literal::Literal, token_kind::TokenKind};
 
 #[derive(Clone)]
 pub enum RuntimeError {
-    InvalidOperandTypes {
+    BadUnOp {
+        operator: TokenKind,
+        right: Literal,
+        line: usize,
+    },
+    BadBinOps {
         left: Literal,
         operator: TokenKind,
         right: Literal,
@@ -12,10 +17,44 @@ pub enum RuntimeError {
     },
 }
 
+pub fn bad_un_op(operator: TokenKind, right: Literal, line: usize) -> RuntimeError {
+    RuntimeError::BadUnOp {
+        operator,
+        right,
+        line,
+    }
+}
+
+pub fn bad_bin_ops(
+    operator: TokenKind,
+    left: Literal,
+    right: Literal,
+    line: usize,
+) -> RuntimeError {
+    RuntimeError::BadBinOps {
+        left,
+        operator,
+        right,
+        line,
+    }
+}
+
 impl fmt::Display for RuntimeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            RuntimeError::InvalidOperandTypes {
+            RuntimeError::BadUnOp {
+                operator,
+                right,
+                line,
+            } => {
+                write!(
+                    f,
+                    "[line {line}] Invalid operand for `{:?}`: `{:?}`",
+                    operator, right
+                )
+            }
+
+            RuntimeError::BadBinOps {
                 left,
                 operator,
                 right,
