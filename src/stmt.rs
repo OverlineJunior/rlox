82be5b916366bb70205d988edd3139b8f573c387
over <franciscosebastianyjunior@hotmail.com>
2024-04-19 19:42:07 -0300

    Add Stmt variant Var

--- src/stmt.rs
+++ src/stmt.rs
@@ -1,7 +1,11 @@
-use crate::expr::Expr;
+use crate::{expr::Expr, token::Token};
 
 #[derive(Debug)]
 pub enum Stmt {
 	Expr(Expr),
 	Print(Expr),
+	Var {
+		name: Token,
+		init: Option<Expr>,
+	},
 }
