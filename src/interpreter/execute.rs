966cf80ee54519e7e47c77d9c5b438033ecbfc08
over <franciscosebastianyjunior@hotmail.com>
2024-04-22 21:40:56 -0300

    Add new parameter for the env and implement variable definition

--- src/interpreter/execute.rs
+++ src/interpreter/execute.rs
@@ -1,9 +1,9 @@
-use super::eval::eval;
-use crate::{error::runtime_error::RuntimeError, stmt::Stmt};
+use super::{env::Env, eval::eval};
+use crate::{error::runtime_error::RuntimeError, literal::Literal, stmt::Stmt};
 
 /// Executes a single statament tree, possibly causing side effects.
 /// This is the statement analogue of `eval`.
-pub fn execute(stmt: Stmt) -> Result<(), RuntimeError> {
+pub fn execute(stmt: Stmt, env: &mut Env) -> Result<(), RuntimeError> {
     match stmt {
         Stmt::Expr(expr) => {
             eval(expr)?;
@@ -11,7 +11,10 @@ pub fn execute(stmt: Stmt) -> Result<(), RuntimeError> {
 
         Stmt::Print(expr) => println!("{}", eval(expr)?),
 
-        Stmt::Var { name, init } => todo!("execute"),
+        Stmt::Var { name, init } => {
+            let value = init.map(eval).transpose()?;
+            env.set(name.lexeme, value);
+        },
     };
 
     Ok(())
