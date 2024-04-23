5d2a1fd13f2f3f7b149b4ae5cab03fc2806d4070
over <franciscosebastianyjunior@hotmail.com>
2024-04-22 21:39:27 -0300

    Fix env methods being private and make set's value field optional

--- src/interpreter/env.rs
+++ src/interpreter/env.rs
@@ -7,18 +7,19 @@ pub struct Env {
 }
 
 impl Env {
-	fn new() -> Self {
+	pub fn new() -> Self {
 		Self {
 			bindings: HashMap::new(),
 		}
 	}
 
-	fn get(&self, name: String) -> Option<&Literal> {
+	pub fn get(&self, name: String) -> Option<&Literal> {
 		self.bindings.get(&name)
 	}
 
-	/// Returns the overwritten literal if there was one.
-	fn set(&mut self, name: String, value: Literal) -> Option<Literal> {
-		self.bindings.insert(name, value)
+	/// Returns the overwritten `Literal` if there was one.
+	/// If `value` is `None`, it is defaulted to `Literal::Nil`.
+	pub fn set(&mut self, name: String, value: Option<Literal>) -> Option<Literal> {
+		self.bindings.insert(name, value.unwrap_or(Literal::Nil))
 	}
 }
