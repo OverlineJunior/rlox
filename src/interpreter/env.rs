cb554bb04133723111d9346922ac2921a1b02542
over <franciscosebastianyjunior@hotmail.com>
2024-08-02 20:36:18 -0300

    Implement Env::assign

--- src/interpreter/env.rs
+++ src/interpreter/env.rs
@@ -3,22 +3,37 @@ use std::collections::HashMap;
 use crate::{literal::Literal, token::Token};
 
 pub struct Env {
-	bindings: HashMap<String, Literal>,
+    bindings: HashMap<String, Literal>,
 }
 
 impl Env {
-	pub fn new() -> Self {
-		Self {
-			bindings: HashMap::new(),
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            bindings: HashMap::new(),
+        }
+    }
 
-	pub fn get(&self, name: &str) -> Option<Literal> {
-		self.bindings.get(name).cloned()
-	}
+    pub fn get(&self, name: &str) -> Option<Literal> {
+        self.bindings.get(name).cloned()
+    }
 
-	/// Defines a new binding or overwrites the old one, returning it.
-	pub fn define(&mut self, name: &str, value: Literal) -> Option<Literal> {
-		self.bindings.insert(name.into(), value)
-	}
+    /// Defines a new binding or overwrites the old one, returning it.
+    pub fn define(&mut self, name: &str, value: Literal) -> Option<Literal> {
+        self.bindings.insert(name.into(), value)
+    }
+
+    /// Assigns a value to an already existing binding, returning the old value.
+    /// Errors if said binding does not exist.
+    pub fn assign(&mut self, name: &str, value: Literal) -> Result<Literal, ()> {
+        if !self.bindings.contains_key(name) {
+            return Err(());
+        }
+
+        let old_value = self
+            .bindings
+            .insert(name.into(), value)
+            .expect("Should have the key");
+
+        Ok(old_value)
+    }
 }
